4a8a4e02f807250ba66bf332182a063126f85a67
basakrdnz <basak.karadeniz0@gmail.com>
2024-08-09 13:05:48 +0300

    updated add card buton and finished

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,28 +1,60 @@
 import React, { useState } from 'react'
 
+
 const Button = () => {
-    const handleClick = () => {
-        const [state, setState] = useState(initialState)
-        console.log("heyy");
+
+  //useState hook
+  const [addCard, setAddCard]=useState(false);
+  const [count, setCount]=useState(0);
+
+  const handleClick = () =>{
+    setAddCard(true);
+  }
+
+  const handleAzalt = () => {
+    if (count>1) {
+    setCount(count-1)
     }
+    else {
+      setAddCard(false)
+    }
+  }
+  
+  const handleArttır = () => {
+    setCount(count + 1);
+  };
+
+
+// {!isAdded ? ( ... ) : ( ... )} koşullu render
+// "ternary operator" denilen üçlü operatör
 
   return (
     <div>
+      {!addCard ?(
     <button 
-    className="absolute bottom-0 left-1/2 transform -translate-x-1/2 translate-y-1/2 flex items-center justify-center w-44 p-2 rounded-3xl bg-white border-2 border-orange-600 font-bold"
-    onClick={handleClick}>
-    <img src="assets\images\icon-add-to-cart.svg" alt="shopping icon" className='px-2' />
-          Add to Cart
-    </button>
+        className="absolute bottom-0 left-1/2 transform -translate-x-1/2 translate-y-1/2 flex items-center justify-center w-44 p-2 rounded-3xl bg-white border-2 border-orange-600 font-bold"
+        onClick={handleClick}
+        >
+        <img src="assets\images\icon-add-to-cart.svg" alt="shopping icon" className='px-2' />
+            Add to Card
+              
+        </button>
+      ):
+      <div
+      className="absolute bottom-0 left-1/2 transform -translate-x-1/2 translate-y-1/2 flex items-center justify-between w-44 p-2 rounded-3xl bg-orange-600 text-white font-bold"
+    >
+      <button onClick={handleAzalt} 
+      className="px-2 border-2 rounded-full hover:bg-white hover:text-orange-600 hover:border-orange-600"
+      >-</button>
+      <div>{count}</div>
+      <button onClick={handleArttır}       
+      className="px-2 border-2 rounded-full hover:bg-white hover:text-orange-600 hover:border-orange-600"
+      >+</button>
+    </div>
+    
+    }
     </div>
   )
 }
 
 export default Button
-
-
-//şimdi burda yapmam gerekenler
-
-//butona tıkladığında başka buton 
-//2. butonun içine iki buton ve bunlarla değişen değer 
-//usestate???araştır
